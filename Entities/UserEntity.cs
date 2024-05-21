9eef0cb503099c9a3a3f945fa40391283761949b
Elkady874 <elkady874@gmail.com>
2024-05-22 02:06:43 +0300

    Add project files.

---
+++ Entities/UserEntity.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Xml.Linq;
+
+namespace BarCLoudTaskBackEnd.Entities
+{
+    [Table("User")]
+    [Index(nameof(Email), IsUnique = true)]
+    public class UserEntity
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+    }
+}
